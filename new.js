1319190358a24fba8339795021d9ad4e62362ece
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2022-01-11 22:05:52 +0600

    init

---
+++ new.js
@@ -0,0 +1,14 @@
+console.log("Heello world");
+
+console.log("HI");
+
+const arr = [1,2,3,4,5,6,7];
+console.log(arr);
+
+const Newarr = [];
+
+console.log(arr.map(element => ++element));
+
+console.log(arr);
+
+console.log(Newarr);
